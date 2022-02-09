fea4c5ef2064eb2d62f7ee14c8cde967a2b63884
ianreve <ianrevecacao@gmail.com>
2022-02-09 01:24:07 +0000

    logo added and dark / ight mode functional

--- js/script.js
+++ js/script.js
@@ -1,76 +1,78 @@
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-	document.documentElement.classList.add('dark')
-  } else {
-	document.documentElement.classList.remove('dark')
-  }
-  
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = 'dark'
-  
-  // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem('theme')
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+// Whenever the user explicitly chooses light mode
+localStorage.theme = "light";
+
+// Whenever the user explicitly chooses dark mode
+localStorage.theme = "dark";
+
+// Whenever the user explicitly chooses to respect the OS preference
+localStorage.removeItem("theme");
 
 //Mobile menu ========================
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
 btn.addEventListener("click", () => {
-	menu.classList.toggle("hidden");
+  menu.classList.toggle("hidden");
 });
 
 // =================Dark Mode Switch ==========
 const checkbox = document.querySelector("#checkbox");
 const html = document.querySelector("html");
-const dot = document.querySelectorAll(".dot")
+const dot = document.querySelectorAll(".dot");
+const logo = document.querySelectorAll(".logo");
 
 const toggleDarkMode = function () {
-  checkbox.checked
-  ? html.classList.add("dark")
-  : html.classList.remove("dark");
-
-}
+  checkbox.checked ? html.classList.add("dark") : html.classList.remove("dark");
+};
 // changing dots colors light and dark mode
-const dots = function(){
+const dots = function () {
   if (checkbox.checked) {
-    dot.forEach(Element => {
-      Element.style.background = "#286d5d"
-    })
+    dot.forEach((Element) => {
+      Element.style.background = "#286d5d";
+    });
+  } else {
+    dot.forEach((Element) => {
+      Element.style.background = "#0A192F";
+    });
   }
-  else{
-    dot.forEach(Element => {
-      Element.style.background = "#0A192F"
-    })
+};
+
+const logoColor = function () {
+  if (checkbox.checked) {
+    logo.forEach((Element) => {
+      Element.style.stroke = "#64ffda";
+    });
+  } else {
+    logo.forEach((Element) => {
+      Element.style.stroke = "#0A192F";
+    });
   }
-  }
-
-
-
-
+};
 
 //calling the function directly
 
 toggleDarkMode();
-checkbox.addEventListener("click",toggleDarkMode);
-checkbox.addEventListener("click",dots);
-
-
-
-
-
-
-
+checkbox.addEventListener("click", toggleDarkMode);
+checkbox.addEventListener("click", dots);
+checkbox.addEventListener("click", logoColor);
 
 // Tytpewritter effect
-const typeWriter = document.getElementById('typewriter-text');
-const text = 'Hi, my name is ';
-
+const typeWriter = document.getElementById("typewriter-text");
+const text = "Hi, my name is ";
 
 typeWriter.innerHTML = text;
-typeWriter.style.setProperty('--characters', text.length);
+typeWriter.style.setProperty("--characters", text.length);
 
 // ================================================================
 // // Get all the elements you want to show on scroll
